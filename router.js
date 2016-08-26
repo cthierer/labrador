b88c7f5a527b31ce3ffc777768f14cbe48663f04
Chris Thierer <cthierer1@gmail.com>
2016-08-25 22:24:54 -0400

    Support CORS pre-flights

--- router.js
+++ router.js
@@ -2,6 +2,18 @@
 const router = require('express').Router();
 const app = require('./src');
 
+router.use(function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', req.headers.origin)
+    res.set('Access-Control-Allow-Headers', 'Content-Type')
+
+    if (req.method.toUpperCase() === 'OPTIONS') {
+        res.status(204).end()
+        return
+    }
+
+    next()
+})
+
 router.get('/', function (req, res, next) {
     res.status(200).send({
         "status": "ok"
